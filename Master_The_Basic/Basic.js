64f50f4a9f0544f2cc402b4f41c8882818bac096
muhammadumar-codes <muhammadumar.codes@gmail.com>
2025-09-19 15:19:06 +0500

    DSA BASIC

---
+++ Master_The_Basic/Basic.js
@@ -0,0 +1,24 @@
+const  x="5";
+let y =5;
+let z=3;
+
+// BODMAS
+
+console.log( `The Add of  x and y is ${x-(y-z*2)}`)
+// 3*2=6
+// 5-6=-1
+// 5-(-1)
+// the final anwer is 6
+
+
+
+
+var oldWay = "I am var";   // Old style (avoid if possible)
+let changeable = "I can change"; // Can be reassigned
+const fixed = "I cannot change"; // Cannot be reassigned
+changeable="i have  changed it";
+console.log(changeable)
+
+
+
+
