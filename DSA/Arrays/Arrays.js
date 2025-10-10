7acc6d6bccec977d69665a8e3646001d5fb8fb6a
muhammadumar-codes <muhammadumar.codes@gmail.com>
2025-10-10 09:13:06 +0500

    update DSA

--- DSA/Arrays/Arrays.js
+++ DSA/Arrays/Arrays.js
@@ -1,5 +1,15 @@
-const  array =[]
-array.push("Hellow")
-array.push(1)
-console.log(array);
+let array = [10, 30, 50, 60, 70];
+let max = Math.max(array[0], array[1]);
+let sMax = Math.min(array[0], array[1]);
 
+for (let i = 2; i <= array.length; i++) {
+  if (array[i] > max) {
+    sMax = max;
+    max = array[i];
+  }
+  else if (array[i] > sMax && max != array[i]) {
+    sMax = array[i];
+  }
+}
+
+console.log(sMax);
