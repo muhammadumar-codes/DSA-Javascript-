4eb7bc5fb610dcd7c2e4b3825849051638bdc00a
muhammadumar-codes <muhammadumar.codes@gmail.com>
2025-10-14 18:52:59 -0700

    Update page

---
+++ DSA/Date/Date.js
@@ -0,0 +1,11 @@
+// in date to get fulll date and time
+let date=new Date();
+console.log(date);
+
+// notes: month ,year start from zero 
+
+
+// gettinn  each day ,month ,fullyear
+
+const month=new Date().setFullYear("2020")
+console.log(month)
