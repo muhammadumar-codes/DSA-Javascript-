f666cb104c36057ea966fed0d4b1d3933445975d
muhammadumar-codes <muhammadumar.codes@gmail.com>
2025-10-25 14:09:34 -0700

    update

--- DSA/Arrays/Arrays.js
+++ DSA/Arrays/Arrays.js
@@ -1,9 +1,36 @@
-let  array=[1,2,3,4]
-let i=0,j=array.length-1;
-while (i<j) {
-  [array[i],array[j]]=[array[j],array[i]]
-  j--;
-  i++;
-  
-}  
-console.log(array)
+// maximum number to check !
+
+let arr=[-1,-2,-3,-4]
+let max=arr[0]
+for(let i=0;i<arr.length; i++){
+  if (arr[i]>max){
+    max=arr[i]
+  }
+}
+console.log(`The maximum value is ${max}`);
+
+
+// Check the minnum number
+
+let arrTwo=[10,20,30,40,50]
+let min=arrTwo[0]
+for(let i=0;i<=arrTwo.length-1; i++){
+  if (arr[i]<min){
+    min=arr[i]
+  }
+}
+console.log(`The Minimum  value is ${min}`);
+
+
+// Check the 2nd largest Number
+
+// let  arrThree=[10,20,30,40,50]
+// let maximum=Math.max(arrThree[0],arrThree[1])
+// let sMax=Math.min(arrThree[0],arrThree[1])
+
+
+// console.log(sMax)
+
+
+
+
